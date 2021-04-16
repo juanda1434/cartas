cfed099f6a11584622559fb7f6604bc0195eae06
Juan David Sanchez Mancilla <marko56_34@hotmail.com>
2021-04-16 08:17:20 -0500

    Se agrego funcion botones, ya suma +1 a cada carta en el localstorage

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict'
-
+const botones= [];
 const getImgCarta = (carta) => {
-    return `<img src="img/${carta.numero}.png" alt="${carta.carta}" style='width:20%'>`;
+    const img = `<img id="carta${carta.numero}" src="img/${carta.numero}.png" alt="${carta.carta}" style='width:20%'>`;
+    botones[botones.length]= {boton:`carta${carta.numero}`,numero:carta.numero};
+    return img;
 }
 
 const getCartas = () => {
@@ -28,25 +30,45 @@ const cargarLocalStorage = () => {
 }
 
 const agregarCarta = (...cartaAgregar) => {
-
+    
     if (cartas != null) {
-        cartas.forEach(carta => {
-
-        })
+        alert("Carta ya fue agregada");
     } else {
         cargarLocalStorage();
     }
 }
 
+const sumarPunto = (numero)=>{
+    const cartas = getCartas()["data"];
+    for (const key in cartas) {
+        if (cartas[key].numero == numero ) {
+            cartas[key].valor = parseInt(cartas[key].valor) +1;
+            
+            localStorage.setItem("cartas",JSON.stringify({data:cartas}));
+            
+        }
+    }
+}
 const cargarImg = () => {
     const cartas = (getCartas())["data"];
     let divCartas = "";
     cartas.forEach(carta => {
-        console.log(carta);
+        //console.log(carta);
         divCartas += getImgCarta(carta);
     });
     document.getElementById("cartas").innerHTML = divCartas;
+    addAcciones();
+}
 
+const addAcciones = (numero)=>{
+
+    botones.forEach(boton => addAccion(boton.boton,()=>{
+        sumarPunto(boton.numero);
+    }));
+}
+const addAccion = (idBtn,accion)=>{
+    document.getElementById(idBtn).onclick = accion;
+    
 }
 
 cargarLocalStorage();
