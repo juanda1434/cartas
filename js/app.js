355f0eb8e9645dae23de125b742bd25c0ba5ea04
Juan David Sanchez Mancilla <marko56_34@hotmail.com>
2021-04-16 07:46:26 -0500

    agregacion logica cargar cartas json para guardarlas en localstorgare y cargar cartas del localstorage al html

--- js/app.js
+++ js/app.js
@@ -1,19 +1,52 @@
-    'use strict'
+'use strict'
 
-    const cargarCartas = ()=>{
-         fetch("https://carlosreneas.github.io/endpoints/cartas.json")
-        .then(r=>r.json())
-        .then(cartas=>{
-            carga.setItem("cartas",cartas);
+const getImgCarta = (carta) => {
+    return `<img src="img/${carta.numero}.png" alt="${carta.carta}" style='width:20%'>`;
+}
+
+const getCartas = () => {
+    return JSON.parse(localStorage.getItem("cartas"));
+}
+
+const cargarCartasJSON = () => {
+
+    fetch("https://carlosreneas.github.io/endpoints/cartas.json")
+        .then(r => r.json())
+        .then(cartas => {
+            localStorage.setItem("cartas", JSON.stringify(cartas));
+
         });
-    }
+}
 
-    const cargarLocalStorage= ()=>{
-        const carga = localStorage.getItem("cartas");
-        if(carga == null){
-            cargarCartas();            
-        }else{
-            
-        }
+const cargarLocalStorage = () => {
+    const carga = localStorage.getItem("cartas");
+    if (carga == null) {
+        cargarCartasJSON();
+    } else {
+        cargarImg();
     }
+}
 
+const agregarCarta = (...cartaAgregar) => {
+
+    if (cartas != null) {
+        cartas.forEach(carta => {
+
+        })
+    } else {
+        cargarLocalStorage();
+    }
+}
+
+const cargarImg = () => {
+    const cartas = (getCartas())["data"];
+    let divCartas = "";
+    cartas.forEach(carta => {
+        console.log(carta);
+        divCartas += getImgCarta(carta);
+    });
+    document.getElementById("cartas").innerHTML = divCartas;
+
+}
+
+cargarLocalStorage();
